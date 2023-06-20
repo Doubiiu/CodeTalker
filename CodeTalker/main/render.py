2d7a1f844de2cbf594d20cf94699f7ba3151f54e
Caspian Ahlberg <caspianahlberg@gmail.com>
2023-06-20 16:48:17 +0200

    Change two file encodings from DOS to UNIX

--- CodeTalker/main/render.py
+++ CodeTalker/main/render.py
@@ -24,181 +24,181 @@ from psbody.mesh import Mesh
 
 
 def add_text_to_image(img, text):
-    # setup text
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    text = text
+	# setup text
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	text = text
 
-    # get boundary of this text
-    textsize = cv2.getTextSize(text, font, 1, 2)[0]
+	# get boundary of this text
+	textsize = cv2.getTextSize(text, font, 1, 2)[0]
 
-    # get coords based on boundary
-    textX = (img.shape[1] - textsize[0]) // 2
-    textY = img.shape[0]- (img.shape[0] + textsize[1]) // 16
+	# get coords based on boundary
+	textX = (img.shape[1] - textsize[0]) // 2
+	textY = img.shape[0]- (img.shape[0] + textsize[1]) // 16
 
-    # add text centered on image
-    cv2.putText(img, text, (textX, textY), font, 1, (255, 255, 255), 2)
-    return img
+	# add text centered on image
+	cv2.putText(img, text, (textX, textY), font, 1, (255, 255, 255), 2)
+	return img
 
 # The implementation of rendering is borrowed from VOCA: https://github.com/TimoBolkart/voca/blob/master/utils/rendering.py
 def render_mesh_helper(args,mesh, t_center, rot=np.zeros(3), tex_img=None, z_offset=0):
-    if args.dataset == "BIWI":
-        camera_params = {'c': np.array([400, 400]),
-                         'k': np.array([-0.19816071, 0.92822711, 0, 0, 0]),
-                         'f': np.array([4754.97941935 / 8, 4754.97941935 / 8])}
-    elif args.dataset == "vocaset":
-        camera_params = {'c': np.array([400, 400]),
-                         'k': np.array([-0.19816071, 0.92822711, 0, 0, 0]),
-                         'f': np.array([4754.97941935 / 2, 4754.97941935 / 2])}
+	if args.dataset == "BIWI":
+		camera_params = {'c': np.array([400, 400]),
+						 'k': np.array([-0.19816071, 0.92822711, 0, 0, 0]),
+						 'f': np.array([4754.97941935 / 8, 4754.97941935 / 8])}
+	elif args.dataset == "vocaset":
+		camera_params = {'c': np.array([400, 400]),
+						 'k': np.array([-0.19816071, 0.92822711, 0, 0, 0]),
+						 'f': np.array([4754.97941935 / 2, 4754.97941935 / 2])}
 
-    frustum = {'near': 0.01, 'far': 3.0, 'height': 800, 'width': 800}
+	frustum = {'near': 0.01, 'far': 3.0, 'height': 800, 'width': 800}
 
-    mesh_copy = Mesh(mesh.v, mesh.f)
-    mesh_copy.v[:] = cv2.Rodrigues(rot)[0].dot((mesh_copy.v-t_center).T).T+t_center
-    
-    intensity = 2.0
+	mesh_copy = Mesh(mesh.v, mesh.f)
+	mesh_copy.v[:] = cv2.Rodrigues(rot)[0].dot((mesh_copy.v-t_center).T).T+t_center
+	
+	intensity = 2.0
 
-    primitive_material = pyrender.material.MetallicRoughnessMaterial(
-                alphaMode='BLEND',
-                baseColorFactor=[0.3, 0.3, 0.3, 1.0],
-                metallicFactor=0.8, 
-                roughnessFactor=0.8 
-            )
+	primitive_material = pyrender.material.MetallicRoughnessMaterial(
+				alphaMode='BLEND',
+				baseColorFactor=[0.3, 0.3, 0.3, 1.0],
+				metallicFactor=0.8, 
+				roughnessFactor=0.8 
+			)
 
 
-    tri_mesh = trimesh.Trimesh(vertices=mesh_copy.v, faces=mesh_copy.f)
+	tri_mesh = trimesh.Trimesh(vertices=mesh_copy.v, faces=mesh_copy.f)
 
-    render_mesh = pyrender.Mesh.from_trimesh(tri_mesh, material=primitive_material,smooth=True)
+	render_mesh = pyrender.Mesh.from_trimesh(tri_mesh, material=primitive_material,smooth=True)
 
-    if args.background_black:
-        scene = pyrender.Scene(ambient_light=[.2, .2, .2], bg_color=[0, 0, 0])
-    else:
-        scene = pyrender.Scene(ambient_light=[.2, .2, .2], bg_color=[255, 255, 255])
-    
-    camera = pyrender.IntrinsicsCamera(fx=camera_params['f'][0],
-                                      fy=camera_params['f'][1],
-                                      cx=camera_params['c'][0],
-                                      cy=camera_params['c'][1],
-                                      znear=frustum['near'],
-                                      zfar=frustum['far'])
+	if args.background_black:
+		scene = pyrender.Scene(ambient_light=[.2, .2, .2], bg_color=[0, 0, 0])
+	else:
+		scene = pyrender.Scene(ambient_light=[.2, .2, .2], bg_color=[255, 255, 255])
+	
+	camera = pyrender.IntrinsicsCamera(fx=camera_params['f'][0],
+									  fy=camera_params['f'][1],
+									  cx=camera_params['c'][0],
+									  cy=camera_params['c'][1],
+									  znear=frustum['near'],
+									  zfar=frustum['far'])
 
-    scene.add(render_mesh, pose=np.eye(4))
+	scene.add(render_mesh, pose=np.eye(4))
 
-    camera_pose = np.eye(4)
-    camera_pose[:3,3] = np.array([0, 0, 1.0-z_offset])
-    scene.add(camera, pose=[[1, 0, 0, 0],
-                            [0, 1, 0, 0],
-                            [0, 0, 1, 1],
-                            [0, 0, 0, 1]])
+	camera_pose = np.eye(4)
+	camera_pose[:3,3] = np.array([0, 0, 1.0-z_offset])
+	scene.add(camera, pose=[[1, 0, 0, 0],
+							[0, 1, 0, 0],
+							[0, 0, 1, 1],
+							[0, 0, 0, 1]])
 
-    angle = np.pi / 6.0
-    pos = camera_pose[:3,3]
-    light_color = np.array([1., 1., 1.])
-    light = pyrender.DirectionalLight(color=light_color, intensity=intensity)
+	angle = np.pi / 6.0
+	pos = camera_pose[:3,3]
+	light_color = np.array([1., 1., 1.])
+	light = pyrender.DirectionalLight(color=light_color, intensity=intensity)
 
-    light_pose = np.eye(4)
-    light_pose[:3,3] = pos
-    scene.add(light, pose=light_pose.copy())
-    
-    light_pose[:3,3] = cv2.Rodrigues(np.array([angle, 0, 0]))[0].dot(pos)
-    scene.add(light, pose=light_pose.copy())
+	light_pose = np.eye(4)
+	light_pose[:3,3] = pos
+	scene.add(light, pose=light_pose.copy())
+	
+	light_pose[:3,3] = cv2.Rodrigues(np.array([angle, 0, 0]))[0].dot(pos)
+	scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] =  cv2.Rodrigues(np.array([-angle, 0, 0]))[0].dot(pos)
-    scene.add(light, pose=light_pose.copy())
+	light_pose[:3,3] =  cv2.Rodrigues(np.array([-angle, 0, 0]))[0].dot(pos)
+	scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] = cv2.Rodrigues(np.array([0, -angle, 0]))[0].dot(pos)
-    scene.add(light, pose=light_pose.copy())
+	light_pose[:3,3] = cv2.Rodrigues(np.array([0, -angle, 0]))[0].dot(pos)
+	scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] = cv2.Rodrigues(np.array([0, angle, 0]))[0].dot(pos)
-    scene.add(light, pose=light_pose.copy())
+	light_pose[:3,3] = cv2.Rodrigues(np.array([0, angle, 0]))[0].dot(pos)
+	scene.add(light, pose=light_pose.copy())
 
-    flags = pyrender.RenderFlags.SKIP_CULL_FACES
-    try:
-        r = pyrender.OffscreenRenderer(viewport_width=frustum['width'], viewport_height=frustum['height'])
-        color, _ = r.render(scene, flags=flags)
-    except:
-        print('pyrender: Failed rendering frame')
-        color = np.zeros((frustum['height'], frustum['width'], 3), dtype='uint8')
+	flags = pyrender.RenderFlags.SKIP_CULL_FACES
+	try:
+		r = pyrender.OffscreenRenderer(viewport_width=frustum['width'], viewport_height=frustum['height'])
+		color, _ = r.render(scene, flags=flags)
+	except:
+		print('pyrender: Failed rendering frame')
+		color = np.zeros((frustum['height'], frustum['width'], 3), dtype='uint8')
 
-    return color[..., ::-1]
+	return color[..., ::-1]
 
 def render_sequence_meshes(args, sequence_vertices, template, out_path, predicted_vertices_path, vt, ft ,tex_img):
-    num_frames = sequence_vertices.shape[0]
-    file_name_pred = predicted_vertices_path.split('/')[-1].split('.')[0]
-    file_name_wav = os.path.join(args.dataset_dir, 'wav', file_name_pred.split('_condition_')[0]+'.wav')
+	num_frames = sequence_vertices.shape[0]
+	file_name_pred = predicted_vertices_path.split('/')[-1].split('.')[0]
+	file_name_wav = os.path.join(args.dataset_dir, 'wav', file_name_pred.split('_condition_')[0]+'.wav')
 
-    tmp_video_file_pred = tempfile.NamedTemporaryFile('w', suffix='.mp4', dir=out_path)
-    writer_pred = cv2.VideoWriter(tmp_video_file_pred.name, cv2.VideoWriter_fourcc(*'mp4v'), args.fps, (800, 800), True) # w, h
+	tmp_video_file_pred = tempfile.NamedTemporaryFile('w', suffix='.mp4', dir=out_path)
+	writer_pred = cv2.VideoWriter(tmp_video_file_pred.name, cv2.VideoWriter_fourcc(*'mp4v'), args.fps, (800, 800), True) # w, h
 
-    center = np.mean(sequence_vertices[0], axis=0)
+	center = np.mean(sequence_vertices[0], axis=0)
 
-    video_fname_pred = os.path.join(out_path, file_name_pred+'.mp4')
-    for i_frame in range(num_frames):
-        render_mesh = Mesh(sequence_vertices[i_frame], template.f)
-        if vt is not None and ft is not None:
-            render_mesh.vt, render_mesh.ft = vt, ft
+	video_fname_pred = os.path.join(out_path, file_name_pred+'.mp4')
+	for i_frame in range(num_frames):
+		render_mesh = Mesh(sequence_vertices[i_frame], template.f)
+		if vt is not None and ft is not None:
+			render_mesh.vt, render_mesh.ft = vt, ft
 
-        pred_img = render_mesh_helper(args,render_mesh, center, tex_img=tex_img)
-        pred_img = pred_img.astype(np.uint8)
+		pred_img = render_mesh_helper(args,render_mesh, center, tex_img=tex_img)
+		pred_img = pred_img.astype(np.uint8)
   
-        writer_pred.write(pred_img)
+		writer_pred.write(pred_img)
 
-    writer_pred.release()
+	writer_pred.release()
 
-    ### render without audio
-    cmd = ('ffmpeg' + ' -i {0} -pix_fmt yuv420p -qscale 0 {1}'.format(
-       tmp_video_file_pred.name, video_fname_pred)).split()
-    call(cmd)
+	### render without audio
+	cmd = ('ffmpeg' + ' -i {0} -pix_fmt yuv420p -qscale 0 {1}'.format(
+	   tmp_video_file_pred.name, video_fname_pred)).split()
+	call(cmd)
 
-    ### render with audio
-    cmd = ('ffmpeg' + ' -i {0} -i {1} -vcodec h264 -ac 2 -channel_layout stereo -qscale 0 {2}'.format(
-       file_name_wav, video_fname_pred, video_fname_pred.replace('.mp4', '_audio.mp4'))).split()
-    call(cmd)
-    if os.path.exists(video_fname_pred):
-        os.remove(video_fname_pred)
+	### render with audio
+	cmd = ('ffmpeg' + ' -i {0} -i {1} -vcodec h264 -ac 2 -channel_layout stereo -qscale 0 {2}'.format(
+	   file_name_wav, video_fname_pred, video_fname_pred.replace('.mp4', '_audio.mp4'))).split()
+	call(cmd)
+	if os.path.exists(video_fname_pred):
+		os.remove(video_fname_pred)
 
 def main():
-    parser = argparse.ArgumentParser(description='CodeTalker: Speech-Driven 3D Facial Animation with Discrete Motion Prior')
-    parser.add_argument("--dataset", type=str, default="vocaset", help='vocaset or BIWI')
-    parser.add_argument("--dataset_dir", type=str, default="./")
-    parser.add_argument('--background_black', type=bool, default=True, help='whether to use black background')
-    parser.add_argument('--fps', type=int,default=30, help='frame rate - 30 for vocaset; 25 for BIWI')
-    parser.add_argument("--vertice_dim", type=int, default=5023*3, help='number of vertices - 5023*3 for vocaset; 23370*3 for BIWI')
-    parser.add_argument("--pred_path", type=str, default='')
-    parser.add_argument("--output_path", type=str, default='')
-    args = parser.parse_args()
-    if args.dataset == 'BIWI':
-        args.fps=25
-        args.vertice_dim=70110
+	parser = argparse.ArgumentParser(description='CodeTalker: Speech-Driven 3D Facial Animation with Discrete Motion Prior')
+	parser.add_argument("--dataset", type=str, default="vocaset", help='vocaset or BIWI')
+	parser.add_argument("--dataset_dir", type=str, default="./")
+	parser.add_argument('--background_black', type=bool, default=True, help='whether to use black background')
+	parser.add_argument('--fps', type=int,default=30, help='frame rate - 30 for vocaset; 25 for BIWI')
+	parser.add_argument("--vertice_dim", type=int, default=5023*3, help='number of vertices - 5023*3 for vocaset; 23370*3 for BIWI')
+	parser.add_argument("--pred_path", type=str, default='')
+	parser.add_argument("--output_path", type=str, default='')
+	args = parser.parse_args()
+	if args.dataset == 'BIWI':
+		args.fps=25
+		args.vertice_dim=70110
 
-    args.dataset_dir = args.dataset_dir + args.dataset
+	args.dataset_dir = args.dataset_dir + args.dataset
 
-    assert os.path.exists(args.pred_path)
-    if not os.path.exists(args.output_path):
-        os.makedirs(args.output_path)
-    else:
-        pass
-        # shutil.rmtree(args.output_path)
-        # os.makedirs(args.output_path)
+	assert os.path.exists(args.pred_path)
+	if not os.path.exists(args.output_path):
+		os.makedirs(args.output_path)
+	else:
+		pass
+		# shutil.rmtree(args.output_path)
+		# os.makedirs(args.output_path)
 
 
-    for file in os.listdir(args.pred_path):
-        if file.endswith("npy"): 
-            predicted_vertices_path = os.path.join(args.pred_path, file)
-            if args.dataset == "BIWI":
-                template_file = os.path.join(args.dataset_dir, "BIWI.ply")
-            elif args.dataset == "vocaset":
-                template_file = os.path.join(args.dataset_dir, "FLAME_sample.ply")
-            print("rendering: ", file)
-        
-            template = Mesh(filename=template_file)
-            vt, ft = None, None
-            tex_img = None
+	for file in os.listdir(args.pred_path):
+		if file.endswith("npy"): 
+			predicted_vertices_path = os.path.join(args.pred_path, file)
+			if args.dataset == "BIWI":
+				template_file = os.path.join(args.dataset_dir, "BIWI.ply")
+			elif args.dataset == "vocaset":
+				template_file = os.path.join(args.dataset_dir, "FLAME_sample.ply")
+			print("rendering: ", file)
+		
+			template = Mesh(filename=template_file)
+			vt, ft = None, None
+			tex_img = None
 
-            predicted_vertices = np.load(predicted_vertices_path)
-            predicted_vertices = np.reshape(predicted_vertices,(-1,args.vertice_dim//3,3))
-            
-            render_sequence_meshes(args, predicted_vertices, template, args.output_path, predicted_vertices_path, vt, ft ,tex_img)
+			predicted_vertices = np.load(predicted_vertices_path)
+			predicted_vertices = np.reshape(predicted_vertices,(-1,args.vertice_dim//3,3))
+			
+			render_sequence_meshes(args, predicted_vertices, template, args.output_path, predicted_vertices_path, vt, ft ,tex_img)
 
 
 if __name__=="__main__":
-    main()
+	main()
